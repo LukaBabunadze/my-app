0c5cc7086fefe4d298b84c875f4ac0971fc06817
LukaBabunadze <luka.babunadze.1@iliauni.edu.ge>
2024-07-25 17:31:43 +0400

    Finishing Server Side Fetching

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,38 +1,49 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
 
-export const posts = [
-  {
-    id: 1,
-    title: "Some Random Title",
-    body: "this is little description for PostCard component",
-    img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    createdAt: new Date(),
-  },
-  {
-    id: 2,
-    title: "Some Random Title",
-    body: "this is little description for PostCard component",
-    img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    createdAt: new Date(),
-  },
-  {
-    id: 3,
-    title: "Some Random Title",
-    body: "this is little description for PostCard component",
-    img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    createdAt: new Date(),
-  },
-  {
-    id: 4,
-    title: "Some Random Title",
-    body: "this is little description for PostCard component",
-    img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    createdAt: new Date(),
-  },
-];
+// export const posts = [
+//   {
+//     id: 1,
+//     title: "Some Random Title",
+//     body: "this is little description for PostCard component",
+//     img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: 2,
+//     title: "Some Random Title",
+//     body: "this is little description for PostCard component",
+//     img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: 3,
+//     title: "Some Random Title",
+//     body: "this is little description for PostCard component",
+//     img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: 4,
+//     title: "Some Random Title",
+//     body: "this is little description for PostCard component",
+//     img: "https://images.pexels.com/photos/1564473/pexels-photo-1564473.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+//     createdAt: new Date(),
+//   },
+// ];
 
-const BlogPage = () => {
+const getPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!res.ok) {
+    throw new Error("Something went wrong!");
+  }
+  return res.json();
+};
+
+const BlogPage = async () => {
+  const posts = await getPosts();
+
   return (
     <div className={styles.container}>
       {posts.map((post) => (
